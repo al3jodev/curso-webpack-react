0d0c7c1549273dd10b5969868ea3b5a16d84090c
al3jodev <ajjm_2184@hotmail.com>
2022-08-26 21:08:08 -0500

    Se agrega npm install -D css-minimizer-webpack-plugin terser-webpack-plugin clean-webpack-plugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,25 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: "/",
+        filename: 'bundle.js'
     },
+    mode: 'development',
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            '@components': path.resolve(__dirname, 'src/components/'),
-            '@styles': path.resolve(__dirname, 'src/styles/')
-        }
+        extensions: ['.js', '.jsx']
     },
-    mode: 'production',
     module: {
         rules: [
             {
@@ -36,9 +28,9 @@ module.exports = {
             {
                 test: /\.s[ac]ss/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
                 ]
             }
         ]
@@ -50,14 +42,13 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
-        }),
-        new CleanWebpackPlugin()
+        })
     ],
-    optimization:{
-        minimize: true,
-        minimizer:[
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3006
     }
 }
